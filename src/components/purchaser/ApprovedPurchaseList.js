7cacb080e75d75aad16c095b4f763f0775c209a7
Linisha <linieesk@gmail.com>
2022-11-10 17:18:31 +0530

    connecting DB and adding form validation

--- src/components/purchaser/ApprovedPurchaseList.js
+++ src/components/purchaser/ApprovedPurchaseList.js
@@ -1,6 +1,109 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { useNavigate, Navigate } from "react-router";
+
+const ApprovedPurchase = (props) => (
+  <tr>
+    <td>{props.ApprovedPurchaseList.poid}</td>
+    <td>{props.ApprovedPurchaseList.supplier}</td>
+    <td>{props.ApprovedPurchaseList.amount}</td>
+    <td>{props.ApprovedPurchaseList.createdon}</td>
+    <td>{props.ApprovedPurchaseList.description}</td>
+    <td>{props.ApprovedPurchaseList.prid}</td>
+  </tr>
+);
+
+const Budget = (props) => (
+  <tr>
+    <td>{props.ApprovedPurchaseList.budget}</td>
+  </tr>
+);
 
 class ApprovedPurchaseList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      approvedPurchase: [],
+      budget: [],
+    };
+  }
+
+  getApprovedPurchase() {
+    axios
+      .get("http://localhost:5000/api/po/get/approved")
+      .then((response) => {
+        this.setState({
+          ApprovedPurchase: response.data,
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  getBudget() {
+    axios
+      .get("http://localhost:5000/api/budget/get/pr")
+      .then((response) => {
+        this.setState({
+          Budget: response.data,
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.ApprovedPurchaseList();
+    this.ViewBudget();
+
+    // axios.get("http://localhost:5000/api/po/get/approved").then((response) => {
+    //   this.setState({
+    //     ApprovedPurchase: response.data,
+    //   });
+    // })
+    // axios
+    //   .get("http://localhost:5000/api/budget/get/pr")
+    //   .then((response) => {
+    //     this.setState({
+    //       Budget: response.data,
+    //     });
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+  }
+
+  ApprovedPurchaseList() {
+    return this.state.approvedPurchase.map(function(
+      currentApprovedPurchase,
+      i
+    ) {
+      return (
+        <ApprovedPurchase approvedPurchase={currentApprovedPurchase} key={i} />
+      );
+    });
+  }
+
+  ViewBudget() {
+    return this.state.budget.map(function (currentBudget, i) {
+      return <Budget budget={currentBudget} key={i} />;
+    });
+  }
+
+  // navigate = useNavigate();
+  // navigateToCreatePurchaseOrder(){
+  //   this.navigate('/CreatePurchaseOrder');
+  // };
+
+  onSubmit() {
+    const { navigate } = this.props;
+    // Navigate to Another Component
+    navigate("/CreatePurchaseOrder");
+  }
+
   render() {
     return (
       <div className="dark-overlay landing-inner text-light">
@@ -9,20 +112,18 @@ class ApprovedPurchaseList extends Component {
             <div className="col-md-12 text-center">
               <h1 className="display-3 mb-4">Budget</h1>
               <hr />
-              <p>populate budget here</p>
+              <p>{this.ViewBudget}</p>
             </div>
           </div>
           <hr />
-          <div class="row">
-            <div class="col-6 col-md-2">
-              <button type="button" class="btn btn-success">
-                Create Purchse Order
-              </button>
+          <div className="row">
+            <div className="col-6 col-md-2">
+              <button className="btn btn-success">Create Purchse Order</button>
             </div>
-            </div>
-            <hr/>
-          <table class="table">
-            <thead class="thead-dark">
+          </div>
+          <hr />
+          <table className="table">
+            <thead className="thead-dark">
               <tr>
                 <th scope="col">#</th>
                 <th scope="col">PR ID</th>
@@ -35,7 +136,7 @@ class ApprovedPurchaseList extends Component {
               </tr>
             </thead>
             <tbody>
-              <tr>
+              {/* <tr>
                 <th scope="row">
                   <div class="form-check form-check-inline">
                     <input
@@ -53,26 +154,8 @@ class ApprovedPurchaseList extends Component {
                 <td>08/11/2022</td>
                 <td>08/11/2022</td>
                 <td>Approved</td>
-              </tr>
-              <tr>
-                <th scope="row">
-                <div class="form-check form-check-inline">
-                    <input
-                      class="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox1"
-                      value="option1"
-                    />
-                  </div>
-                </th>
-                <td>PR-001</td>
-                <td>PR Name</td>
-                <td>Blah blah</td>
-                <td>LKR 4,000</td>
-                <td>08/11/2022</td>
-                <td>08/11/2022</td>
-                <td>Approved</td>
-              </tr>
+              </tr> */}
+              {this.ApprovedPurchaseList()}
             </tbody>
           </table>
         </div>
